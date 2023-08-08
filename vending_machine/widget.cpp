53fbc29966886a11b68814e921d249258601ca6b
byObin <byobin26@gmail.com>
2023-08-08 11:57:55 -0700

    button activate func done

--- vending_machine/widget.cpp
+++ vending_machine/widget.cpp
@@ -16,71 +16,67 @@ Widget::Widget(QWidget *parent)
     ui->pbMilk->setEnabled(false);
 }
 
-Widget::~Widget()
-{
+Widget::~Widget(){
     delete ui;
 }
 
-void Widget::changeMoney(int diff)
-{
+void Widget::changeMoney(int diff){
     money += diff;
 
+     // Update money display
     ui->lcdNumber->display(money);
-    // ui->pbCoffee->setEnabled(false);
-}
 
-void Widget::disactivateDrinkButton(int diff)
-{
-    if (money + diff > 100) {
+    // Update drink button states based on money
+    if (money >= 100) {
+        ui->pbCoffee->setEnabled(true);
+    } else {
         ui->pbCoffee->setEnabled(false);
         ui->pbTea->setEnabled(false);
         ui->pbMilk->setEnabled(false);
-    } else {
-        ui->pbCoffee->setEnabled(true);
-        ui->pbTea->setEnabled(true);
-        ui->pbMilk->setEnabled(true);
     }
+
+    if (money >= 150) {
+        ui->pbTea->setEnabled(true);
+    }
+
+    if (money >= 200) {
+        ui->pbMilk->setEnabled(true);
+    }  
 }
 
-void Widget::on_pb10_clicked()
-{
+void Widget::on_pb10_clicked(){
     changeMoney(10);
 }
 
-void Widget::on_pb50_clicked()
-{
+void Widget::on_pb50_clicked(){
     changeMoney(50);
 }
 
-void Widget::on_pb100_clicked()
-{
+void Widget::on_pb100_clicked(){    
     changeMoney(100);
 }
 
-void Widget::on_pb500_clicked()
-{
+void Widget::on_pb500_clicked(){
     changeMoney(500);
 }
 
-void Widget::on_pbCoffee_clicked()
-{
-    changeMoney(-100);
+void Widget::on_pbCoffee_clicked(){
+    if (money >= 100) {
+        changeMoney(-100);
+    }
 }
 
 
-void Widget::on_pbTea_clicked()
-{
+void Widget::on_pbTea_clicked(){
     changeMoney(-150);
 }
 
 
-void Widget::on_pbMilk_clicked()
-{
+void Widget::on_pbMilk_clicked(){
     changeMoney(-200);
 }
 
-void Widget::on_pbRst_clicked()
-{
+void Widget::on_pbRst_clicked(){
     QMessageBox mb;
     mb.information(nullptr, "Title", "Text");
 }
